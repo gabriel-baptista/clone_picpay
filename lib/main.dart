6ba188a8e3079dc5e1f51500b0ec9a39b58feae1
gabriel-baptista <gbdso16@gmail.com>
2021-04-29 18:32:31 -0300

    commit on main

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Clone PicPay',
       theme: ThemeData(
         // This is the theme of your application.
